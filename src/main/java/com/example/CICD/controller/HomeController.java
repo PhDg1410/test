09de051c542e23aa1a958cf255d6f54db06c02b6
PhDg1410 <duongvip1410@gmail.com>
2023-12-20 23:49:00 +0700

    last test

--- src/main/java/com/example/CICD/controller/HomeController.java
+++ src/main/java/com/example/CICD/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/hello")
     public String Hello() {
-        return "hello world in my new container";
+        return "hello world last version";
     }
 }
